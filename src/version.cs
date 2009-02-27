c2a9bec3e45accb80d0500409ebb99b31fa9fbe8
Jon Jacky <jon@u.washington.edu>
2009-02-27 21:21:38 +0000

    Fix build configuration: src\version.cs assign current release version 1.0.21029.0 not previous 1.0.20430.0, src\Mp2Dot\Mp2Dot.csproj: reference NModel project here, not installed NModel.dll

--- src/version.cs
+++ src/version.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("NModel")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2007")]
-[assembly: AssemblyVersion("1.0.20430.0")]
-[assembly: AssemblyFileVersion("1.0.20430.0")]
+[assembly: AssemblyVersion("1.0.21029.0")]
+[assembly: AssemblyFileVersion("1.0.21029.0")]
